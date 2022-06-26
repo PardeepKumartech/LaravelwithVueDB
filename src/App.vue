3997929775736cc0dd77e591062f19e7b904f14e
pardeepkumartech <pardeep_kumar93@hotmail.com>
2022-06-26 23:45:54 +0500

    Fruits todo added

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return a.createdAt - b.createdAt;
+  })
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  {
+    deep: true,
+  }
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() === "") {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    done: false,
+    editable: false,
+    createdAt: new Date().getTime(),
+  });
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
+</script>
+
+<template>
+  <main class="app">
+    <h2 class="title">Fruits Todo List</h2>
+
+    <section class="create-todo">
+      <form id="new-todo-form" @submit.prevent="addTodo">
+        <input
+          type="text"
+          name="content"
+          id="content"
+          placeholder="Add Fruits"
+          v-model="input_content"
+        />
+
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list" id="todo-list">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`"
+        >
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble fruits`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
